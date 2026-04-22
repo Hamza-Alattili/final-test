f9faed5775665736c3490f843532b17f469c2f65
Hamza <hamzaalattili@gmail.com>
2026-04-22 22:49:28 +0300

    .

---
+++ Application/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    }
+}
